9544eae46c397ce65f95c07343eb921cf48a7fdf
Cliff Fajardo <cliff.fajardo@gmail.com>
2025-02-13 23:53:52 -0800

    Show loading skeletons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -11,7 +11,7 @@ interface Props {
 const GameCard = ({ game }: Props) => {
 
     return (
-        <Card.Root borderRadius={'4px'}>
+        <Card.Root width={'300px'} borderRadius={'10'} overflow={'hidden'}>
             <Image src={getCroppedImageUrl(game.background_image)} />
             <Card.Body>
                 <Heading size={"2xl"}>{game.name}</Heading>
---
+++ src/components/GameCardSkeleton.tsx
@@ -0,0 +1,16 @@
+import { Card, Heading, HStack, Image, Skeleton } from '@chakra-ui/react'
+import { SkeletonText } from './ui/skeleton'
+
+const GameCardSkeleton = () => {
+    return (
+        <Card.Root width={'300px'} borderRadius={'10'} overflow={'hidden'}>
+            <Skeleton height={'200px'} />
+            <Card.Body>
+                <SkeletonText />
+            </Card.Body>
+
+        </Card.Root>
+    )
+}
+
+export default GameCardSkeleton
